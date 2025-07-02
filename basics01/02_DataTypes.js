04f4f0e11941f1d3de3521bf7e010d711f5f2ba2
Muhammad Owais Qarni <168034844+mowaisqarni@users.noreply.github.com>
2025-07-02 19:02:38 +0000

    Data Types

---
+++ basics01/02_DataTypes.js
@@ -0,0 +1,22 @@
+"use strict" // treat all code as new version3
+//alert (3+3 ) // we are using nodejs , not browser
+
+
+
+let st="i am owais " // string 
+let a=12    // int
+let b=false // boolean 
+
+// number => 2 to power 53
+// Bigint
+// boolean => true/false
+// null standalone value
+// undefined e.g( let a;)
+// symbol =>unique
+
+console.log(typeof "owais")
+console.log(typeof a);
+console.log(typeof udefined)
+console.log(typeof null);
+
+
