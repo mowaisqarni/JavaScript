be4158558067d69798c52545fbe8ffbb0b250484
Muhammad Owais Qarni <168034844+mowaisqarni@users.noreply.github.com>
2025-07-02 18:42:08 +0000

    add

---
+++ basics01/01variables.js
@@ -0,0 +1,9 @@
+console.log("Hello world")
+const id="123"
+var name="owais"
+let city="FSD"
+rollno ="3058"
+let accountstate;
+console.table([id,name,city,rollno])
+rollno="8792"
+console.table([id,name,city,rollno,accountstate])
