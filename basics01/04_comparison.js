0e8bf89e6de61882cf18f26e141f449d710ea8c5
Muhammad Owais Qarni <168034844+mowaisqarni@users.noreply.github.com>
2025-07-02 20:12:12 +0000

    comparison

---
+++ basics01/04_comparison.js
@@ -0,0 +1,12 @@
+console.log(null>=0)
+console.log(null==0)
+console.log(null<0)
+
+console.log(undefined>=0)
+console.log(undefined==0)
+console.log(undefined<0)
+
+// all these above comparison are unpredicatable did not use 
+
+// === strictly check for values with datatypes
+console.log("2"==="2")
