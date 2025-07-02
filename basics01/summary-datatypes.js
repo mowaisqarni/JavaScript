e9c0eb1b808d43d6e72b84256e5b204d45c78e7b
Muhammad Owais Qarni <168034844+mowaisqarni@users.noreply.github.com>
2025-07-02 21:41:41 +0000

    summary data types

---
+++ basics01/summary-datatypes.js
@@ -0,0 +1,22 @@
+const score = 100;
+const scoreValue = 100.3;
+
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail;
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+  console.log(id===anotherId);
+
+  const heros=["M-bin-Qasim" , "ErtugulGhazi" , "Ibn-Mugheera"]
+
+  let myobj = {
+     name:"Badar-Bin-Mugheera" ,
+    age:12
+  }
+  const myfun =function (){
+    console.log("Hello world");
+  }
+  console.log(typeof myobj);
+  // read this for help
